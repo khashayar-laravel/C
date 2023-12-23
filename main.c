d6cda5bd103fc71a90bf1ad7f6e59538477fa7e0
KhashayarPourakpour <147341475+khashayar-laravel@users.noreply.github.com>
2023-12-23 14:53:08 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int prime_array[50]; 
+    int index = 0;
+    
+    for(int numbers = 0; numbers<=100; ++numbers)
+    {
+        if(numbers==1)
+        {
+            continue;
+        }
+        
+        if(numbers==2 || numbers==3 || numbers==5 || numbers==7 
+        || numbers==11)
+        {
+            prime_array[index]=numbers;
+            printf("%d\n",numbers);
+            ++index;
+        }
+        
+        if(numbers % 2 !=0 && numbers % 3 !=0 && numbers % 5 !=0 
+        && numbers % 7 !=0 && numbers % 11 !=0)
+        {
+            prime_array[index]=numbers;
+            printf("%d\n",numbers);
+            ++index;
+        }
+    }
+   
+}
